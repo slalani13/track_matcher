5669c1d1608c4a0bb9a7e9b92a4a9e0426c2efec
rishijs <rishijs2002@gmail.com>
2025-02-18 18:38:10 -0500

    home page and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title: String = "angular-whos-who";
+
+  ngOnInit() {
+    /*
+    localStorage.getItem("theme")
+    can get local storage
+    */
+    document.documentElement.classList.add("dark");
+    document.documentElement.classList.add("bg-slate-800");
+    document.documentElement.classList.add("text-white");
+  }
+
 }
