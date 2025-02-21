6d06e1a5c2a82f80946e55fa680d3a287c3f75c5
Shariq Lalani <slalani13@gmail.com>
2025-02-20 18:46:57 -0600

    updated add to leaderboard

--- src/app/Components/leaderboard/leaderboard.component.ts
+++ src/app/Components/leaderboard/leaderboard.component.ts
@@ -29,7 +29,7 @@ export class LeaderboardComponent implements OnInit{
 
   loadLeaderboard(): void {
     this.leaderboard = this.getLeaderboard();
-    this.leaderboard.sort((a, b) => b.score - a.score);
+    // this.leaderboard.sort((a, b) => b.score - a.score);
     console.log("Leaderboard loaded:", this.leaderboard);
   }
 
@@ -40,8 +40,16 @@ export class LeaderboardComponent implements OnInit{
 
   addToLeaderboard(username: string, newscore: number): void {
     const leaderboard = this.getLeaderboard();
+    this.leaderboard.sort((a, b) => b.score - a.score);
+    if (this.leaderboard.length >= 5) {
+      // pop last entry if less than newscore and add new entry
+      if (leaderboard[leaderboard.length - 1].score < newscore) {
+        leaderboard.pop();
+      }
+    } 
     const entry: LeaderboardEntry = { name: username, score: newscore };
     leaderboard.push(entry);
+    leaderboard.sort((a, b) => b.score - a.score);
     localStorage.setItem(LEADERBOARD_KEY, JSON.stringify(leaderboard));
   }
 
